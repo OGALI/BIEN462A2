d0ffba852eb4721d7bc54be893491723fce42240
Ali Najmaldin <ali.najmaldin@gmail.com>
2019-02-19 17:56:54 -0500

    quick

--- BIEN462A2.m
+++ BIEN462A2.m
@@ -1,7 +1,7 @@
 %% Question 1 a)
 clear all, close all
 n = -100000:100000;
-xn = cos(pi/7*10^3*n)+ 1/2*cos(pi/6*10^3*n);
+xn = cos(pi/7*n)+ 1/2*cos(pi/6*n);
 % xcorr(xn,xn);
 % plot(xn)
 % crosscorr(xn,xn)
@@ -12,29 +12,57 @@ title('Autocorrelation Sequence')
 xlabel('Time Lag')
 ylabel('Sample Autocorrelation')
 grid on
+
+%% Question 1 b)
+i = 1;
+for L = [10 30 100 150 300 600]
+    Fs = 10^4;            % Sampling frequency                    
+    T = 1/Fs;             % Sampling period       
+    t = (0:L-1);        % Time vector
+    
+    figure(1)
+    X = cos(pi/7*t)+ 1/2*cos(pi/6*t);
+    Y = fft(X);
+    P2 = abs(Y/L);
+    P1 = P2(1:L/2+1);
+    P1(2:end-1) = 2*P1(2:end-1);
+    f = Fs*(0:(L/2))/L;
+    subplot(3,2,i)
+    plot(f,P1) 
+    title('Single-Sided Amplitude Spectrum of X(t) L='+string(L))
+    xlabel('f (Hz)')
+    ylabel('|P1(f)|')
+    
+    % Plots the signal itself
+    figure(2)
+    subplot(3,2,i)
+    plot(Fs*t(1:L-1),X(1:L-1));
+    title('The Signal itself with L=' + string(L));
+    xlabel('t (milliseconds)');
+    ylabel('X(t)');
+    
+    i = i+1;
+end
+
 %% Question 1 b)
 clear all, close all
-% n = 0:100;
-% xn = cos(pi/7*10^3*n)+ 1/2*cos(pi/6*10^3*n);
-% 
-% periodagram(xn)
 
-Fs = 10;            % Sampling frequency                    
+Fs = 10^4;            % Sampling frequency                    
 T = 1/Fs;             % Sampling period       
-L = 10;             % Length of signal
+L = 100;             % Length of signal
 t = (0:L-1)*T;        % Time vector
 X = cos(pi/7*10^4*t)+ 1/2*cos(pi/6*10^4*t);
 
-% Plots the signal itself
-figure(1)
-subplot(3,2,1)
-plot(Fs*t(1:L-1),X(1:L-1));
-title('Signal Corrupted with Zero-Mean Random Noise N=10')
-xlabel('t (milliseconds)')
-ylabel('X(t)')
+% % Plots the signal itself
+% figure(1)
+% subplot(3,2,1)
+% plot(Fs*t(1:L-1),X(1:L-1));
+% title('Signal Corrupted with Zero-Mean Random Noise N=10');
+% xlabel('t (milliseconds)');
+% ylabel('X(t)');
 
 figure(2)
-Y = fft(X)
+Y = fft(X);
 P2 = abs(Y/L);
 P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
@@ -45,12 +73,14 @@ title('Single-Sided Amplitude Spectrum of X(t) L=10')
 xlabel('f (Hz)')
 ylabel('|P1(f)|')
 
-
+clear all
 figure(2)
-L = 11;             % Length of signal
+Fs = 10^4;            % Sampling frequency                    
+T = 1/Fs;             % Sampling period       
+L = 1000;             % Length of signal
 t = (0:L-1)*T;        % Time vector
 X = cos(pi/7*10^3*t)+ 1/2*cos(pi/6*10^3*t);
-Y = fft(X)
+Y = fft(X);
 P2 = abs(Y/L);
 P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
@@ -61,19 +91,22 @@ title('Single-Sided Amplitude Spectrum of X(t) L=11')
 xlabel('f (Hz)')
 ylabel('|P1(f)|')
 
-% Plots the signal itself
-figure(1)
-subplot(3,2,2)
-plot(Fs*t(1:L-1),X(1:L-1));
-title('Signal Corrupted with Zero-Mean Random Noise N=11')
-xlabel('t (milliseconds)')
-ylabel('X(t)')
+% % Plots the signal itself
+% figure(1)
+% subplot(3,2,2)
+% plot(Fs*t(1:L-1),X(1:L-1));
+% title('Signal Corrupted with Zero-Mean Random Noise N=11')
+% xlabel('t (milliseconds)')
+% ylabel('X(t)')
 
+clear all
 figure(2)
-L = 30;             % Length of signal
+Fs = 10^4;            % Sampling frequency                    
+T = 1/Fs;             % Sampling period       
+L = 10000;             % Length of signal
 t = (0:L-1)*T;        % Time vector
 X = cos(pi/7*10^3*t)+ 1/2*cos(pi/6*10^3*t);
-Y = fft(X)
+Y = fft(X);
 P2 = abs(Y/L);
 P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
@@ -84,19 +117,22 @@ title('Single-Sided Amplitude Spectrum of X(t) L=20')
 xlabel('f (Hz)')
 ylabel('|P1(f)|')
 
-% Plots the signal itself
-figure(1)
-subplot(3,2,3)
-plot(Fs*t(1:L-1),X(1:L-1));
-title('Signal Corrupted with Zero-Mean Random Noise N=30')
-xlabel('t (milliseconds)')
-ylabel('X(t)')
+% % Plots the signal itself
+% figure(1)
+% subplot(3,2,3)
+% plot(Fs*t(1:L-1),X(1:L-1));
+% title('Signal Corrupted with Zero-Mean Random Noise N=30')
+% xlabel('t (milliseconds)')
+% ylabel('X(t)')
 
+clear all
 figure(2)
-L = 50;             % Length of signal
+Fs = 10^4;            % Sampling frequency                    
+T = 1/Fs;             % Sampling period       
+L = 10000000;             % Length of signal
 t = (0:L-1)*T;        % Time vector
 X = cos(pi/7*10^3*t)+ 1/2*cos(pi/6*10^3*t);
-Y = fft(X)
+Y = fft(X);
 P2 = abs(Y/L);
 P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
@@ -107,64 +143,69 @@ title('Single-Sided Amplitude Spectrum of X(t) L=50')
 xlabel('f (Hz)')
 ylabel('|P1(f)|')
 
-% Plots the signal itself
-figure(1)
-subplot(3,2,4)
-plot(Fs*t(1:L-1),X(1:L-1));
-title('Signal Corrupted with Zero-Mean Random Noise N=50')
-xlabel('t (milliseconds)')
-ylabel('X(t)')
+% % Plots the signal itself
+% figure(1)
+% subplot(3,2,4);
+% plot(Fs*t(1:L-1),X(1:L-1));
+% title('Signal Corrupted with Zero-Mean Random Noise N=50');
+% xlabel('t (milliseconds)');
+% ylabel('X(t)');
 
+clear all
 figure(2)
-L = 100;             % Length of signal
+Fs = 10^4;            % Sampling frequency                    
+T = 1/Fs;             % Sampling period       
+L = 1000000;             % Length of signal
 t = (0:L-1)*T;        % Time vector
 X = cos(pi/7*10^3*t)+ 1/2*cos(pi/6*10^3*t);
-Y = fft(X)
+Y = fft(X);
 P2 = abs(Y/L);
 P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
 f = Fs*(0:(L/2))/L;
-subplot(3,2,5)
-plot(f,P1) 
-title('Single-Sided Amplitude Spectrum of X(t) L=100')
-xlabel('f (Hz)')
-ylabel('|P1(f)|')
+subplot(3,2,5);
+plot(f,P1) ;
+title('Single-Sided Amplitude Spectrum of X(t) L=100');
+xlabel('f (Hz)');
+ylabel('|P1(f)|');
 
-% Plots the signal itself
-figure(1)
-subplot(3,2,5)
-plot(Fs*t(1:L-1),X(1:L-1));
-title('Signal Corrupted with Zero-Mean Random Noise N=100')
-xlabel('t (milliseconds)')
-ylabel('X(t)')
+% % Plots the signal itself
+% figure(1)
+% subplot(3,2,5)
+% plot(Fs*t(1:L-1),X(1:L-1));
+% title('Signal Corrupted with Zero-Mean Random Noise N=100');
+% xlabel('t (milliseconds)');
+% ylabel('X(t)');
 
-
+clear all
 figure(2)
-L = 500;             % Length of signal
+Fs = 10^4;            % Sampling frequency                    
+T = 1/Fs;             % Sampling period       
+L = 10^9;             % Length of signal
 t = (0:L-1)*T;        % Time vector
 X = cos(pi/7*10^3*t)+ 1/2*cos(pi/6*10^3*t);
-Y = fft(X)
+Y = fft(X);
 P2 = abs(Y/L);
 P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
 f = Fs*(0:(L/2))/L;
-subplot(3,2,6)
-plot(f,P1) 
-title('Single-Sided Amplitude Spectrum of X(t) L=500')
-xlabel('f (Hz)')
-ylabel('|P1(f)|')
+subplot(3,2,6);
+plot(f,P1) ;
+title('Single-Sided Amplitude Spectrum of X(t) L=500');
+xlabel('f (Hz)');
+ylabel('|P1(f)|');
 
-% Plots the signal itself
-figure(1)
-subplot(3,2,6)
-plot(Fs*t(1:L-1),X(1:L-1));
-title('Signal Corrupted with Zero-Mean Random Noise N=500')
-xlabel('t (milliseconds)')
-ylabel('X(t)')
+% % Plots the signal itself
+% figure(1)
+% subplot(3,2,6);
+% plot(Fs*t(1:L-1),X(1:L-1));
+% title('Signal Corrupted with Zero-Mean Random Noise N=500');
+% xlabel('t (milliseconds)');
+% ylabel('X(t)');
 
 %% Question 1 c)
 clear all, close all
-Fs = 10;            % Sampling frequency                    
+Fs = 10^4;            % Sampling frequency                    
 T = 1/Fs;             % Sampling period       
 L = 1500;             % Length of signal
 t = (0:L-1)*T;        % Time vector
@@ -189,7 +230,7 @@ grid on
 
 %% Question 1 d)
 clear all, close all
-Fs = 10;            % Sampling frequency                    
+Fs = 10^4;            % Sampling frequency                    
 T = 1/Fs;             % Sampling period       
 L = 1500;             % Length of signal
 t = (0:L-1)*T;        % Time vector
